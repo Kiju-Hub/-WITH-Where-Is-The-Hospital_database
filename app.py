6dad24450bf96c3b4af7840b9d3bb6091070343f
Kiju-Hub <qkrrlwn97@naver.com>
2025-12-02 17:25:30 +0900

    배포 1차

--- app.py
+++ app.py
@@ -397,4 +397,8 @@ def chat_bot():
 # 서버 실행
 # ================================
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(
+        debug=False,
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
